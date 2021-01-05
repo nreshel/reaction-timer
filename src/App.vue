b1a10f929a7788195c73c84f3de666f3f4833b17
Rod Eshel <rodeshel@Rods-MacBook-Pro.local>
2021-01-05 16:34:08 -0500

    added reaction time to game

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying === true" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
+</template>
+
+<script>
+import Block from './components/Block'
+import Results from './components/Results'
+export default {
+  name: 'App',
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false;
+      console.log(this.delay);
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>Reaction Time: {{score}} ms</p>
+  <p>{{response}}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      response: null
+    }
+  },
+  mounted() {
+    if(this.score < 250) {
+      this.response = "Ninja Reflexes"
+    } else if(this.score < 400) {
+      this.response = "Rapid Reflexes"
+    } else {
+      this.response = "Snail Reflexes"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
